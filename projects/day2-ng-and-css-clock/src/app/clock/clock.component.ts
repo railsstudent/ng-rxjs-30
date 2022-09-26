92315817d757de03ae4734c19556d6e66508bfaf
Connie Leung <cleung334@gmail.com>
2022-09-27 01:22:16 +0800

    refactor: css transform observable

--- projects/day2-ng-and-css-clock/src/app/clock/clock.component.ts
+++ projects/day2-ng-and-css-clock/src/app/clock/clock.component.ts
@@ -1,50 +1,58 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { map, Observable, shareReplay, tap, timer } from 'rxjs';
+import {  map, Observable, take, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-clock',
   template: `
-    <div class="clock">
+    <div class="clock" *ngIf="clockHandsTransform$ | async as clockHandsTransform">
       <div class="clock-face">
-        <div class="hand hour-hand"></div>
-        <div class="hand min-hand"></div>
-        <div class="hand second-hand"></div>
+        <div class="hand hour-hand">{{ clockHandsTransform.secondHandTransform }}</div>
+        <div class="hand min-hand">{{ clockHandsTransform.minuteHandTransform }}</div>
+        <div class="hand second-hand">{{ clockHandsTransform.hourHandTransform }}</div>
       </div>
-      Testing
     </div>
   `,
   styleUrls: ['./clock.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClockComponent implements OnInit {
-  // secondHandDegree$: Observable<string>;
+  clockHandsTransform$!: Observable<{
+    secondHandTransform: string;
+    minuteHandTransform: string;
+    hourHandTransform: string;
+  }>
   
   ngOnInit(): void {
     const oneSecond = 1000;
+
     const currentTime$ = timer(0, oneSecond)
       .pipe(
         map(() => new Date()),
         tap((time) => console.log('currentTime fired', time)),
-        shareReplay(1)
       );
 
-    currentTime$.pipe(
-      tap(time => console.log('seconds', time)),
-      map(time => time.getSeconds())
-    )
-    .subscribe(seconds => console.log(seconds));
+    const timeParts$ = currentTime$
+      .pipe(
+        map(time => ({ 
+          seconds: time.getSeconds(),
+          minutes: time.getMinutes(),
+          hours: time.getHours()
+        }),
+      ));
 
-    currentTime$.pipe(
-      tap(time => console.log('minutes', time)),
-      map(time => time.getMinutes())
-    )
-    .subscribe(seconds => console.log(seconds));
+    this.clockHandsTransform$ = timeParts$
+      .pipe(
+        map(({ seconds, minutes, hours }) => { 
+          const secondsDegrees = ((seconds / 60) * 360) + 90;
+          const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+          const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
 
-
-    currentTime$.pipe(
-      tap(time => console.log('hours', time)),
-      map(time => time.getHours())
-    )
-    .subscribe(seconds => console.log(seconds));
+          return { 
+            secondHandTransform: `rotate(${secondsDegrees}deg)`,
+            minuteHandTransform: `rotate(${minsDegrees}deg)`,
+            hourHandTransform: `rotate(${hourDegrees}deg)`
+          };
+        }),
+      );
   }
 }
