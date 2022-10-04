565083446be6f605c07980fc0dbdc08ae5d4e26f
Connie Leung <cleung334@gmail.com>
2022-10-05 00:25:58 +0800

    feat: implement logic of panels and panel component

--- projects/day5-flex-panel-gallery/src/app/panels/panel/panel.component.ts
+++ projects/day5-flex-panel-gallery/src/app/panels/panel/panel.component.ts
@@ -1,26 +1,48 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
+import { filter, fromEvent, map, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-panel',
   template: `
-    <div class="panel" #panel>
-      <ng-content></ng-content>
-    </div>
+    <ng-content></ng-content>
   `,
   styleUrls: ['./panel.component.scss']
 })
-export class PanelComponent implements OnInit {
-
-  @ViewChild('panel', { static: true })
-  panel!: ElementRef<HTMLDivElement>;
+export class PanelComponent implements OnInit, OnDestroy {
   
   @Input()
   backgroundImage!: string;
 
-  constructor() {}
+  @HostBinding('class.open')  isOpen = false;
+
+  @HostBinding('class.open-active')  isOpenActive = false;
+  
+  subscription: Subscription = new Subscription();
+
+  constructor(private hostElement: ElementRef) {}
 
   ngOnInit(): void {
-    this.panel.nativeElement.style.setProperty('background-image', `url(${this.backgroundImage})`);
+    this.hostElement.nativeElement.style.setProperty('background-image', `url(${this.backgroundImage})`);
+
+    this.subscription.add(
+      fromEvent(this.hostElement.nativeElement, 'click')
+        .pipe(tap(() => this.isOpen = !this.isOpen))
+        .subscribe()
+    );
+    
+    this.subscription.add(
+      fromEvent(this.hostElement.nativeElement, 'transitionend')
+        .pipe(
+          filter(evt => evt instanceof TransitionEvent),
+          map(evt => evt as TransitionEvent),
+          filter(evt => evt.propertyName.includes('flex')),
+          tap(() => this.isOpenActive = !this.isOpenActive)
+        )
+        .subscribe()
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
