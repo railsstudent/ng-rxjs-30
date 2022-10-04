114cdd7e92e654606b2d4c157efe6e3785cfdff2
Connie Leung <cleung334@gmail.com>
2022-10-04 23:04:56 +0800

    feat: implement logic of panels and panel component

--- projects/day5-flex-panel-gallery/src/app/panels/panel/panel.component.ts
+++ projects/day5-flex-panel-gallery/src/app/panels/panel/panel.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
-  templateUrl: './panel.component.html',
+  template: `
+    <div class="panel" #panel>
+      <ng-content></ng-content>
+    </div>
+  `,
   styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('panel', { static: true })
+  panel!: ElementRef<HTMLDivElement>;
+  
+  @Input()
+  backgroundImage!: string;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.panel.nativeElement.style.setProperty('background-image', `url(${this.backgroundImage})`);
   }
 
 }
--- projects/day5-flex-panel-gallery/src/app/panels/panels/panels.component.ts
+++ projects/day5-flex-panel-gallery/src/app/panels/panels/panels.component.ts
@@ -1,11 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { PanelData } from './interfaces';
 
 @Component({
   selector: 'app-panels',
-  templateUrl: './panels.component.html',
+  template: `
+    <div class="panels">
+      <app-panel *ngFor="let panelData of data" [backgroundImage]="panelData.backgroundImage">
+        <p>{{ panelData.headline1 }}</p>
+        <p>{{ panelData.headline2 }}</p>
+        <p>{{ panelData.headline3 }}</p>
+      </app-panel>
+    </div>
+  `,
   styleUrls: ['./panels.component.scss']
 })
 export class PanelsComponent implements OnInit {
+
+  data: PanelData[] = [ 
+    {
+      headline1: 'Hey',
+      headline2: 'Let\'s',
+      headline3: 'Dance',
+      backgroundImage: 'https://source.unsplash.com/gYl-UtwNg_I/1500x1500',
+    },
+    {
+      headline1: 'Give',
+      headline2: 'Take',
+      headline3: 'Receive',
+      backgroundImage: 'https://source.unsplash.com/rFKUFzjPYiQ/1500x1500',
+    },
+    {
+      headline1: 'Experience',
+      headline2: 'It',
+      headline3: 'Today',
+      backgroundImage: 'https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d',
+    },
+    {
+      headline1: 'Give',
+      headline2: 'All',
+      headline3: 'You can',
+      backgroundImage: 'https://source.unsplash.com/ITjiVXcwVng/1500x1500',
+    },
+    {
+      headline1: 'Life',
+      headline2: 'In',
+      headline3: 'Motion',
+      backgroundImage: 'https://source.unsplash.com/3MNzGlQM7qs/1500x1500',
+    }
+  ]
 
   constructor() { }
 
