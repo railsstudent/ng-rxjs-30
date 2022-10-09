a6f4d51d3c851a56c267931927e22d2f093d529b
Connie Leung <cleung334@gmail.com>
2022-10-10 01:34:20 +0800

    refactor: use decimal pipe to format population

--- projects/day6-ng-type-ahead/src/app/type-ahead/pipes/highlight-suggestion.pipe.ts
+++ projects/day6-ng-type-ahead/src/app/type-ahead/pipes/highlight-suggestion.pipe.ts
@@ -10,18 +10,11 @@ export class HighlightSuggestionPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
   transform(suggestion: City, value: string): unknown {
-    const { state, population, city } = suggestion
+    const { state, city } = suggestion
     const regex = new RegExp(value, 'gi');
     const cityName = city.replace(regex, `<span class="hl">${value}</span>`);
     const stateName = state.replace(regex, `<span class="hl">${value}</span>`);
 
-    return this.sanitizer.bypassSecurityTrustHtml(`
-      <span class="name">${cityName}, ${stateName}</span>
-      <span class="population">${this.numberWithCommas(population)}</span>
-    `);
-  }
-
-  private numberWithCommas(population: string): string {
-    return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return this.sanitizer.bypassSecurityTrustHtml(`${cityName}, ${stateName}`);
   }
 }
