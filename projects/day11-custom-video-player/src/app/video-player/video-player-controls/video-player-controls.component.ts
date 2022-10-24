8388171b541e425e6e15615809617ba3c0ed907f
Connie Leung <cleung334@gmail.com>
2022-10-25 00:15:30 +0800

    feat: video player and video player controls

--- projects/day11-custom-video-player/src/app/video-player/video-player-controls/video-player-controls.component.ts
+++ projects/day11-custom-video-player/src/app/video-player/video-player-controls/video-player-controls.component.ts
@@ -2,7 +2,18 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-video-player-controls',
-  templateUrl: './video-player-controls.component.html',
+  template: `
+    <div class="player__controls">
+      <div class="progress">
+        <div class="progress__filled"></div>
+      </div>
+      <button class="player__button toggle" title="Toggle Play">►</button>
+      <input type="range" name="volume" class="player__slider" min="0" max="1" step="0.05" value="1">
+      <input type="range" name="playbackRate" class="player__slider" min="0.5" max="2" step="0.1" value="1">
+      <button data-skip="-10" class="player__button">« 10s</button>
+      <button data-skip="25" class="player__button">25s »</button>
+    </div>
+  `,
   styleUrls: ['./video-player-controls.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
--- projects/day11-custom-video-player/src/app/video-player/video-player/video-player.component.ts
+++ projects/day11-custom-video-player/src/app/video-player/video-player/video-player.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
-  templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss'],
+  template: `
+    <div class="player">
+      <video class="player__video viewer" currentTime="10">
+        <source [src]="videoSrc" type="video/mp4">
+      </video>
+      <app-video-player-controls></app-video-player-controls>
+    </div>
+  `,
+  styleUrls: ['video-player.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VideoPlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(APP_BASE_HREF) private baseHref: string) { }
 
   ngOnInit(): void {
   }
 
+  get videoSrc(): string {
+    const isEndWithSlash = this.baseHref.endsWith('/');
+    return `${this.baseHref}${isEndWithSlash ? '' : '/'}assets/652333414.mp4`;
+  }
 }
