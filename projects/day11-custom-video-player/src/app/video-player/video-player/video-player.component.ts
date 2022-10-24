14b62ffdaee72c47f41ce9e0a3c2b8684ffd6a19
Connie Leung <cleung334@gmail.com>
2022-10-25 01:55:34 +0800

    feat: add rxjs to video

--- projects/day11-custom-video-player/src/app/video-player/video-player/video-player.component.ts
+++ projects/day11-custom-video-player/src/app/video-player/video-player/video-player.component.ts
@@ -1,13 +1,13 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { Component, OnInit, ChangeDetectionStrategy, Inject, ViewChild, ElementRef } from '@angular/core';
-import { fromEvent, map, merge, Observable, startWith, tap } from 'rxjs';
+import { fromEvent, map, merge, Observable, startWith, Subscription, tap } from 'rxjs';
 import { VideoPlayerService } from '../services';
 
 @Component({
   selector: 'app-video-player',
   template: `
     <div class="player">
-      <video class="player__video viewer" currentTime="10" #videoPlayer>
+      <video class="player__video viewer" currentTime="10" #video>
         <source [src]="videoSrc" type="video/mp4">
       </video>
       <app-video-player-controls></app-video-player-controls>
@@ -18,9 +18,33 @@ import { VideoPlayerService } from '../services';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  constructor(@Inject(APP_BASE_HREF) private baseHref: string) { }
+  @ViewChild('video', { static: true })
+  video!: ElementRef<HTMLVideoElement>;
+
+  subscription = new Subscription();
+
+  constructor(@Inject(APP_BASE_HREF) private baseHref: string, private videoService: VideoPlayerService) { }
 
   ngOnInit(): void {
+    const videoNativeElement = this.video.nativeElement;
+    const videoClicked$ = fromEvent(videoNativeElement, 'click')
+      .pipe(map(() => videoNativeElement.paused ? 'play' : 'pause'));
+
+    const videoButtonIcon$ = 
+      merge(
+        fromEvent(videoNativeElement, 'play').pipe(map(() => '►')), 
+        fromEvent(videoNativeElement, 'pause').pipe(map(() => '❚ ❚')),
+      )
+
+    const videoProgress$ = fromEvent(videoNativeElement, 'timeupdate')
+      .pipe(map(() => (videoNativeElement.currentTime / videoNativeElement.duration) * 100));
+        
+        
+      //   .pipe(map(() => false));  
+
+    // this.videoService.isPlayerHover$ = merge(hover$, notHover$).pipe(startWith(true));
+
+    // this.videoPlayer.nativeElement.play();
   }
 
   get videoSrc(): string {
