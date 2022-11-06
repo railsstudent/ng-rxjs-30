51584c5f03aba3108f2d63f520ea9b2db389bf02
Connie Leung <cleung334@gmail.com>
2022-11-06 22:37:19 +0800

    feat: scroll component

--- projects/day13-slide-in-on-scroll/src/app/scroll/scroll/scroll.component.ts
+++ projects/day13-slide-in-on-scroll/src/app/scroll/scroll/scroll.component.ts
@@ -3,7 +3,43 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-scroll',
   templateUrl: './scroll.component.html',
-  styleUrls: ['./scroll.component.scss'],
+  styles: [`
+  .site-wrap {
+    max-width: 700px;
+    margin: 100px auto;
+    background: white;
+    padding: 40px;
+    text-align: justify;
+  }
+
+  .align-left {
+      float: left;
+      margin-right: 20px;
+
+      &.slide-in {
+          transform: translateX(-30%) scale(0.95);
+      }
+  }
+
+  .align-right {
+      float: right;
+      margin-left: 20px;
+
+      .slide-in {
+          transform: translateX(30%) scale(0.95);
+      }
+  }
+
+  .slide-in {
+      opacity: 0;
+      transition: all .5s;
+
+      &.active {
+          opacity: 1;
+          transform: translateX(0%) scale(1); 
+      }
+  }  
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScrollComponent implements OnInit {
