77ef47e821d036d0aa5a61a9607dc6b802b3dc46
Connie Leung <cleung334@gmail.com>
2022-11-23 21:03:06 +0800

    feat: mouse move shadow

--- projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
+++ projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
@@ -2,8 +2,30 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-mouse-move',
-  templateUrl: './mouse-move.component.html',
+  template: `
+    <div class="hero">
+      <h1 contenteditable>🔥WOAH!</h1>
+    </div>
+  `,
   styleUrls: ['./mouse-move.component.scss'],
+  styles: [`
+    :host { 
+      display: block;
+    }
+
+    .hero {
+      min-height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: black;
+    }
+
+    h1 {
+      text-shadow: 10px 10px 0 rgba(0,0,0,1);
+      font-size: 100px;
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MouseMoveComponent implements OnInit {
