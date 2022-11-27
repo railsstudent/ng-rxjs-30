3425b31b6d2f4b77e42dcd5f43792c119f1c6ae5
Connie Leung <cleung334@gmail.com>
2022-11-27 17:32:29 +0800

    chore: delete commented codes

--- projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
+++ projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
@@ -46,13 +46,6 @@ export class MouseMoveComponent implements OnInit {
         filter(e => e instanceof MouseEvent),
         map(e => e as MouseEvent),
         mapTextShadowStyle(nativeElement),
-        // map(({ xWalk, yWalk }) => 
-        //   (`
-        //     ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
-        //     ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
-        //     ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
-        //     ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
-        // `)),
         startWith('')
       ); 
   }
