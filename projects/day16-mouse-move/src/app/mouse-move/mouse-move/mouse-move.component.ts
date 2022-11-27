bf280983b88ae947fbb034b3295583f72798756a
Connie Leung <cleung334@gmail.com>
2022-11-27 17:14:40 +0800

    refactor: mapTextShadowStyle custom operator

--- projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
+++ projects/day16-mouse-move/src/app/mouse-move/mouse-move/mouse-move.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 import { filter, fromEvent, map, Observable, startWith } from 'rxjs';
-import { mapXYWalk } from '../custom-operators/mapXYWalk.operator';
+import { mapTextShadowStyle } from '../custom-operators/mapTextShadowStyle.operator';
 
 @Component({
   selector: 'app-mouse-move',
@@ -45,14 +45,14 @@ export class MouseMoveComponent implements OnInit {
       .pipe(
         filter(e => e instanceof MouseEvent),
         map(e => e as MouseEvent),
-        mapXYWalk(nativeElement),
-        map(({ xWalk, yWalk }) => 
-          (`
-            ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
-            ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
-            ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
-            ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
-        `)),
+        mapTextShadowStyle(nativeElement),
+        // map(({ xWalk, yWalk }) => 
+        //   (`
+        //     ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+        //     ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+        //     ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+        //     ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+        // `)),
         startWith('')
       ); 
   }
