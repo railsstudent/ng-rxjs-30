8be737ada9775b211357d3557965d9d2a99c49d2
Connie Leung <cleung334@gmail.com>
2022-12-03 08:54:55 +0800

    refactor: add barrel file

--- projects/day17-sorted-without-articles/src/app/app.module.ts
+++ projects/day17-sorted-without-articles/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListModule } from './list/list.module';
+import { ListModule } from './list';
 
 @NgModule({
   declarations: [
