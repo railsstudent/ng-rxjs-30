7b822f19061a83a5943c8b412d9a82a636e4860b
Connie Leung <cleung334@gmail.com>
2022-12-10 23:28:10 +0800

    chore: rename to accumulator

--- projects/day18-adding-up-times/src/app/videos/custom-operators/average-video.operator.ts
+++ projects/day18-adding-up-times/src/app/videos/custom-operators/average-video.operator.ts
@@ -1,11 +1,11 @@
 import { map, Observable, reduce } from 'rxjs';
 
-export function averageVideoTime<T>(sumFunction: (acc: number, x: T) => number) {
+export function averageVideoTime<T>(accumulator: (acc: number, x: T) => number) {
     return function (source: Observable<T>) {
         return source.pipe(
             reduce((acc, item: T) => 
                 ({ 
-                    total: sumFunction(acc.total, item),
+                    total: accumulator(acc.total, item),
                     count: acc.count + 1
                 }), 
                 { total: 0, count: 0 }),
