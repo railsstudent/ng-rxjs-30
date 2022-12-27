f582087e156a09db2c37edb507e1c7cc7d7db329
Connie Leung <cleung334@gmail.com>
2022-12-27 23:58:42 +0800

    feat: use css variable to update dynamic image

--- projects/day30-wrack-a-mole/src/app/game/mole/mole.component.ts
+++ projects/day30-wrack-a-mole/src/app/game/mole/mole.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-mole',
@@ -6,87 +7,46 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
     <h1>Whack-a-mole! <span class="score">0</span></h1>
     <button onClick="startGame()">Start!</button>
     <div class="game">
-      <div class="hole hole1">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole2">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole3">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole4">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole5">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole6">
-        <div class="mole"></div>
+      <div *ngFor="let id of [1, 2, 3, 4, 5, 6]" class="hole hole{{id}}" [style]="'--hole-image:' + holeSrc">
+        <div class="mole" [style]="'--mole-image:' + moleSrc"></div>
       </div>
     </div>`,
-  styles: [`
-    h1 {
-      text-align: center;
-      font-size: 10rem;
-      line-height: 1;
-      margin-bottom: 0;
-    }
-
-    .score {
-      background: rgba(255,255,255,0.2);
-      padding: 0 3rem;
-      line-height: 1;
-      border-radius: 1rem;
-    }
-
-    .game {
-      width: 600px;
-      height: 400px;
-      display: flex;
-      flex-wrap: wrap;
-      margin: 0 auto;
-    }
-
-    .hole {
-      flex: 1 0 33.33%;
-      overflow: hidden;
-      position: relative;
-    }
-
-    .hole:after {
-      display: block;
-      background: url(dirt.svg) bottom center no-repeat;
-      background-size: contain;
-      content: '';
-      width: 100%;
-      height:70px;
-      position: absolute;
-      z-index: 2;
-      bottom: -30px;
-    }
-
-    .mole {
-      background: url('mole.svg') bottom center no-repeat;
-      background-size: 60%;
-      position: absolute;
-      top: 100%;
-      width: 100%;
-      height: 100%;
-      transition:all 0.4s;
-    }
-
-    .hole.up .mole {
-      top: 0;
-    }
-  `],
+  styleUrls: ['mole.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MoleComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(APP_BASE_HREF) private baseHref: string) { }
 
   ngOnInit(): void {
   }
 
+  get moleSrc(): string {
+    return this.buildImage('mole.svg');
+  }
+
+  get holeSrc(): string {
+    return this.buildImage('dirt.svg');
+  }
+
+  private buildImage(image: string) {
+    const isEndWithSlash = this.baseHref.endsWith('/');
+    const imagePath = `${this.baseHref}${isEndWithSlash ? '' : '/'}assets/images/${image}`;
+    return `url('${imagePath}')`
+  }
+
+  // randomTime(min: number, max: number): number {
+  //   return Math.round(Math.random() * (max - min) + min);
+  // }
+
+  // randomHole(holes) {
+  //   const idx = Math.floor(Math.random() * holes.length);
+  //   const hole = holes[idx];
+  //   if (hole === lastHole) {
+  //     console.log('Ah nah thats the same one bud');
+  //     return this.randomHole(holes);
+  //   }
+  //   lastHole = hole;
+  //   return hole;
+  // }
 }
