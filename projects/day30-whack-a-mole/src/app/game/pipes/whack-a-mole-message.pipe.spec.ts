10df1125a17339e97676ac91d7f45c42dd0fe049
Connie Leung <cleung334@gmail.com>
2022-12-31 13:25:48 +0800

    fix: typos

--- projects/day30-whack-a-mole/src/app/game/pipes/whack-a-mole-message.pipe.spec.ts
+++ projects/day30-whack-a-mole/src/app/game/pipes/whack-a-mole-message.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { WhackAMoleMessagePipe } from './whack-a-mole-message.pipe';
 
-describe('AlertMessagePipe', () => {
+describe('WhackAMoleMessagePipe', () => {
   it('create an instance', () => {
     const pipe = new WhackAMoleMessagePipe();
     expect(pipe).toBeTruthy();
