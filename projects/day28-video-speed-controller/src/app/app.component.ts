3e1139c6b544b8fdfb6a8b4bf766194ffb411635
Connie Leung <cleung334@gmail.com>
2023-01-01 21:20:13 +0800

    feat: day28 video speed controller

---
+++ projects/day28-video-speed-controller/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-video-player></app-video-player>',
+  styles: [`
+  :host {
+    display: block;
+  }
+  `]
+})
+export class AppComponent {
+  title = 'Day28 Video Speed Controller';
+
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
+}
---
+++ projects/day28-video-speed-controller/src/app/video/video-player/video-player.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class VideoPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
