6710abb3965274964525892469886e25f3ccc420
Connie Leung <cleung334@gmail.com>
2023-01-04 00:08:18 +0800

    chore: import module from barrel file

--- projects/day28-video-speed-controller/src/app/app.module.ts
+++ projects/day28-video-speed-controller/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { VideoModule } from './video/video.module'
+import { VideoModule } from './video'
 
 @NgModule({
   declarations: [
