53702bc354f26b1cf6976516d62e62b28920daf9
Connie Leung <cleung334@gmail.com>
2023-01-12 08:17:16 +0800

    feat: drag and drop (WIP)

--- projects/day27-click-and-drag/src/app/slider/slider/slider.component.ts
+++ projects/day27-click-and-drag/src/app/slider/slider/slider.component.ts
@@ -1,24 +1,71 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { concatMap, filter, fromEvent, map, merge, Subscription, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-slider',
   template: `
-    <div class="items">
-      <div *ngFor="let index of sliders" class="item">{{index}}</div>
+    <div class="items" #items>
+      <div *ngFor="let index of panels" class="item">{{index}}</div>
     </div>
   `,
   styleUrls: ['./slider.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit, OnDestroy {
 
-  sliders = [...Array(25).keys()].map(i => i < 9 ? `0${i + 1}` : `${i + 1}`);
+  @ViewChild('items', { static: true, read: ElementRef })
+  slider!: ElementRef<HTMLDivElement>;
+
+  panels = [...Array(25).keys()].map(i => i < 9 ? `0${i + 1}` : `${i + 1}`);
+
+  subscription = new Subscription();
 
   constructor() { }
 
   ngOnInit(): void {
+    const sliderNative = this.slider.nativeElement;
+    const mouseDown$ = fromEvent(sliderNative, 'mousedown')
+      .pipe(
+        tap(() => sliderNative.classList.add('active')),
+        tap(() => console.log('mouse down'))
+      );
+    const mouseLeave$ = fromEvent(sliderNative, 'mousedown');
+    const mouseUp$ = fromEvent(sliderNative, 'mousedown');
+    const stop$ = merge(mouseLeave$, mouseUp$)
+      .pipe(
+        tap(() => sliderNative.classList.remove('active')),
+        tap(() => console.log('stop dragging')),
+      );
+    const mouseMove$ = fromEvent(sliderNative, 'mousemove')
+      .pipe(takeUntil(stop$));
+
+    this.subscription.add(
+      mouseDown$.pipe(
+        filter((moveDownEvent) => moveDownEvent instanceof MouseEvent),
+        map((moveDownEvent) => moveDownEvent as MouseEvent),
+        concatMap((moveDownEvent) => {
+          const startX = moveDownEvent.pageX - sliderNative.offsetLeft;
+          const scrollLeft = sliderNative.scrollLeft;          
+          return mouseMove$.pipe(
+            // tap(() => console.log('mousemove')),
+            filter((moveEvent) => moveEvent instanceof MouseEvent),
+            map((moveEvent) => moveEvent as MouseEvent),
+            tap((moveEvent) => { 
+              console.log('e', moveEvent);
+              moveEvent.preventDefault();
+            }),
+            map((e) => {
+              const x = e.pageX - sliderNative.offsetLeft;
+              const walk = (x - startX) * 3;
+              sliderNative.scrollLeft = scrollLeft - walk;
+            }),
+          );
+        })
+      ).subscribe()
+    );
   }
 
-
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
