2316ecffa9e0a5f409c817846ea8bc78d6f073b6
Connie Leung <cleung334@gmail.com>
2023-01-24 19:52:33 +0800

    refactor: barrel file for slider module

--- projects/day27-click-and-drag/src/app/app.module.ts
+++ projects/day27-click-and-drag/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SliderModule } from './slider/slider.module';
+import { SliderModule } from './slider';
 
 @NgModule({
   declarations: [
