3c48bbc50a23ed2f05554b996224992c3d4d6084
Connie Leung <cleung334@gmail.com>
2023-01-27 00:43:47 +0800

    feat: stripe module

--- projects/day26-stripe-follow-along-nav/src/app/app.module.ts
+++ projects/day26-stripe-follow-along-nav/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StripeModule } from './stripe';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StripeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
