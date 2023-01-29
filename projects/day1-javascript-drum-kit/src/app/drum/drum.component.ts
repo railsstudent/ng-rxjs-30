f3ecc9b1dfd9bbeb68cc957bc4908d9b3d4dde42
Connie Leung <cleung334@gmail.com>
2023-01-29 23:36:37 +0800

    refactor:  day1-javascript-drum-kit

--- projects/day1-javascript-drum-kit/src/app/drum/drum.component.ts
+++ projects/day1-javascript-drum-kit/src/app/drum/drum.component.ts
@@ -1,6 +1,7 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ElementRef, Inject, OnDestroy, OnInit } from '@angular/core';
-import { filter, fromEvent, map, Subscription, tap } from 'rxjs';
+import { filter, fromEvent, map, Subscription } from 'rxjs';
+import { WINDOW } from '../core';
 import { DrumService } from '../services';
 import { ENTRIES } from './drum.constant';
 
@@ -11,39 +12,61 @@ import { ENTRIES } from './drum.constant';
       <app-drum-key *ngFor="let entry of entries" [entry]="entry" class="key"></app-drum-key>
     </div>
   `,
-  styleUrls: ['./drum.component.scss'],
+  styles: [`
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      height: 100%;
+
+      background-size: cover;
+      background-position-x: center;
+      background-position-y: bottom;
+    }
+
+    div.keys {
+      max-width: 100%;
+      display: inline-flex;
+
+      .key {
+        flex: 1;
+      }
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DrumComponent implements OnInit, OnDestroy {
-  subscription: Subscription | undefined;
+  subscription!: Subscription;
   entries = ENTRIES;
 
-  constructor(private hostElement: ElementRef, private drumService: DrumService, @Inject(APP_BASE_HREF) private baseHref: string) {}
+  constructor(private hostElement: ElementRef<HTMLElement>,
+    private drumService: DrumService,
+    @Inject(APP_BASE_HREF) private baseHref: string,
+    @Inject(WINDOW) private window: Window,
+  ) {}
 
   ngOnInit(): void {
     const allowedKeys = this.entries.map(entry => entry.key)
-    this.subscription = fromEvent(window, 'keydown')
+    this.subscription = fromEvent(this.window, 'keydown')
       .pipe(
         filter(evt => evt instanceof KeyboardEvent),
         map(evt => evt as KeyboardEvent),
         map(({ key }) => key.toUpperCase()),
         filter(key => allowedKeys.includes(key)),
-        tap(key => this.drumService.playSound(key))
       )
-      .subscribe();
+      .subscribe((key) => this.drumService.playSound(key));
 
-    this.hostElement.nativeElement.style['background-image'] = this.imageUrl;
+    this.hostElement.nativeElement.style.backgroundImage = this.imageUrl;
   }
 
   get imageUrl() {
     const isEndWithSlash = this.baseHref.endsWith('/');
-    const image =  `${this.baseHref}${ isEndWithSlash ? '' : '/' }assets/images/background.jpg`; 
+    const image =  `${this.baseHref}${ isEndWithSlash ? '' : '/' }assets/images/background.jpg`;
     return `url('${image}')`;
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.subscription.unsubscribe();
   }
 }
