11cce2b9c140dcc5720a57c8a88ed24d93790359
Connie Leung <cleung334@gmail.com>
2023-02-12 21:21:07 +0800

    refactor: use standalone components

--- projects/day2-ng-and-css-clock/src/app/app.component.ts
+++ projects/day2-ng-and-css-clock/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ClockComponent } from './clock';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    ClockComponent
+  ],
   template: '<app-clock></app-clock>',
   styles: [`
     :host {
       display: block;
     }
-  `]
+  `],
 })
 export class AppComponent {
   constructor(titleService: Title) {
--- projects/day2-ng-and-css-clock/src/app/clock/clock.component.ts
+++ projects/day2-ng-and-css-clock/src/app/clock/clock.component.ts
@@ -1,9 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import {  map, Observable, timer } from 'rxjs';
-import { HandTransformations } from './clock.interface';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { currentTime, rotateClockHands } from './custom-operators/clock.operator';
 
 @Component({
   selector: 'app-clock',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    NgIf,
+  ],
   template: `
     <div class="clock" *ngIf="clockHandsTransform$ | async as clockHandsTransform">
       <div class="clock-face">
@@ -13,33 +19,61 @@ import { HandTransformations } from './clock.interface';
       </div>
     </div>
   `,
-  styleUrls: ['./clock.component.scss'],
+  styles: [
+    `:host {
+      background: #018DED url(https://unsplash.it/1500/1000?image=881&blur=5);
+      background-size: cover;
+  
+      margin: 0;
+      font-size: 2rem;
+      display: flex;
+      flex: 1;
+      min-height: 100vh;
+      align-items: center;
+    }
+    
+    .clock {
+      width: 30rem;
+      height: 30rem;
+      border: 20px solid white;
+      border-radius: 50%;
+      margin: 50px auto;
+      position: relative;
+      padding: 2rem;
+      box-shadow:
+        0 0 0 4px rgba(0,0,0,0.1),
+        inset 0 0 0 3px #EFEFEF,
+        inset 0 0 10px black,
+        0 0 10px rgba(0,0,0,0.2);
+    }
+    
+    .clock-face {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      transform: translateY(-3px); /* account for the height of the clock hands */
+    }
+    
+    .hand {
+      width: 50%;
+      height: 6px;
+      background: black;
+      position: absolute;
+      top: 50%;
+      transform-origin: 100%;
+      transform: rotate(90deg);
+      transition: all 0.05s;
+      transition-timing-function: cubic-bezier(0.1, 2.7, 0.58, 1);
+    }`
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClockComponent {
   readonly oneSecond = 1000;
 
-  clockHandsTransform$: Observable<HandTransformations> = 
-    timer(0, this.oneSecond)
-      .pipe(
-        map(() => { 
-          const time = new Date();
-          return { 
-            seconds: time.getSeconds(),
-            minutes: time.getMinutes(),
-            hours: time.getHours()
-          }
-        }),
-        map(({ seconds, minutes, hours }) => { 
-          const secondsDegrees = ((seconds / 60) * 360) + 90;
-          const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
-          const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
-
-          return { 
-            secondHandTransform: `rotate(${secondsDegrees}deg)`,
-            minuteHandTransform: `rotate(${minsDegrees}deg)`,
-            hourHandTransform: `rotate(${hourDegrees}deg)`
-          };
-        }),
-      );
+  clockHandsTransform$ = timer(0, this.oneSecond)
+    .pipe(
+      currentTime(),
+      rotateClockHands(),
+    );
 }
