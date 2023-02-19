adda666e35ac213e70c38b0668ce75ef8c7738d6
Connie Leung <cleung334@gmail.com>
2023-02-19 15:17:45 +0800

    refactor: remove inboxItems and get message from Subject

--- projects/day10-hold-shift-and-check-checkboxes/src/app/inbox/inbox/inbox.component.ts
+++ projects/day10-hold-shift-and-check-checkboxes/src/app/inbox/inbox/inbox.component.ts
@@ -5,38 +5,16 @@ import { InboxItemComponent } from '../inbox-item/inbox-item.component';
 import { CheckboxClickState, InBetweenCheckboxClicked } from '../interfaces';
 import { MessageService } from '../services';
 
-// const createMessageFn = () => {
-//   const messageService = inject(MessageService);
-
-//   return (checkboxClickedSub$: Subject<CheckboxClickState>) => {
-//     return checkboxClickedSub$.pipe(
-//       scan((acc, item) =>  ({ ...item, prevCheck: acc.lastCheck }),
-//       {
-//         isShiftKeyPressed: false,
-//         isChecked: false,
-//         prevCheck: -1,
-//         lastCheck: -1,
-//       }),
-//       map(({ isShiftKeyPressed, isChecked, prevCheck, lastCheck }) => {
-//         messageService.updateMessageState(lastCheck, isChecked);
-//         checkBetweenBoxes(lastCheck, prevCheck, isShiftKeyPressed, isChecked);
-//         return messageService.getMessages();
-//       }),
-//       startWith(messageService.getMessages())
-//     );
-//   }
-// }
-
 const checkBetweenBoxesFn = () => {
   const messageService = inject(MessageService);
 
-  return (inboxItems: InboxItemComponent[], inBetweenClicked: InBetweenCheckboxClicked) => {
+  return (inBetweenClicked: InBetweenCheckboxClicked) => {
     const { isShiftKeyPressed, isChecked, lastCheck, prevCheck } = inBetweenClicked;
     
     if (isShiftKeyPressed && isChecked) {
       let inBetween = false;
-      inboxItems.forEach(inboxItem => {
-        const id = inboxItem.data.id;
+      const messages = messageService.getMessages();
+      messages.forEach(({ id }) => {
         if (id === lastCheck || id === prevCheck) {
           inBetween = !inBetween;
         }
@@ -101,7 +79,7 @@ export class InboxComponent {
       } as InBetweenCheckboxClicked),
       map((inBetweenClicked) => {
         this.messageService.updateMessageState(inBetweenClicked.lastCheck, inBetweenClicked.isChecked);
-        this.checkBetweenBoxes(this.inboxItems, inBetweenClicked);
+        this.checkBetweenBoxes(inBetweenClicked);
         return this.messageService.getMessages();
       }),
       startWith(this.messageService.getMessages())
