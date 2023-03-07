882dc4e37c4c2a263ce9ac694674f90ab290310e
Connie Leung <cleung334@gmail.com>
2023-03-07 08:41:12 +0800

    feat: speech detection app using standalone component

---
+++ projects/day20-speech-detection-standalone/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { SpeechDetectionComponent } from './speech-detection/speech-detection/speech-detection.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    SpeechDetectionComponent
+  ],
+  template: '<app-speech-detection></app-speech-detection>',
+  styles: [`
+    :host {
+      display: block;
+    }
+  `]
+})
+export class AppComponent {
+  title = 'Day20 Speech Detection Standalone';
+
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
+}
---
+++ projects/day20-speech-detection-standalone/src/app/speech-detection/interfaces/speech-recognition.interface.ts
@@ -0,0 +1,7 @@
+export interface SpeechRecognitionInfo {
+    transcript: string;
+    confidence: number;
+    isFinal: boolean;
+}
+
+export type Transcript = Omit<SpeechRecognitionInfo, 'isFinal' | 'confidence'> & { confidencePercentage: string; };
